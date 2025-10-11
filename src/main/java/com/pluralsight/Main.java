c2d2aea2a17144c2e83e67c0aec686faddc42c96
origin <acmuhammadmustafa@gmail.com>
2025-10-10 22:50:39 -0400

    menus complete, not output though.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,8 @@
 package com.pluralsight;
 
+import jdk.jshell.spi.ExecutionControl;
+
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
@@ -9,11 +12,9 @@ public class Main {
     public static void main(String[] args) {
         String command;
         do {
-            System.out.println("What would you like to do?");
-            System.out.println(" D) Add Deposit"); // Will prompt for debit info and save it to the csv file.
-            System.out.println(" P) Make Payment (Debit)"); // Will prompt for debit info and save it to the csv file.
-            System.out.println(" L) Ledger"); // Displays ledger menu.
-            System.out.println(" X) Exit"); // Quits/Exits application.
+            String homeMenu = "What would you like to do?\n D) Add Deposit\n P) Make Payment (Debit)\n L) Ledger\n X) Exit\n";
+            System.out.println(homeMenu);
+
             command = ConsoleHelper.promptForString("Enter your command");
             switch (command) {
                 case "D":
@@ -56,7 +57,7 @@ public class Main {
                     System.out.println("Invalid input. Please enter a valid option.");
                     System.out.println("======================");
             }
-        } while (command != "X");
+        } while (command != "X" || command !="x");
         }
 
 
@@ -67,15 +68,157 @@ public class Main {
     private static void makePayment() {
         System.out.println("makePayment works.");
     }
-
+// Ledgers Menu: ------------------------------------------------------------------------------------------------------------------------------------
     private static void ledgerMenu() {
-        System.out.println("ledgerMenu works.");
+        System.out.println("Loading ledger: ");
+        String ledgerMenu = "Choose an option: \n A) All\n D) Deposits\n P) Payments\n R) Reports\n H) Home\n";
+        String ledgerCommand;
+        do{
+            System.out.println(ledgerMenu);
+            ledgerCommand = ConsoleHelper.promptForString("Enter your command");
+            switch (ledgerCommand){
+                case "A":
+                    displayEntries();
+                    System.out.println("======================");
+                    break;
+
+                case "D":
+                    displayDEntries();
+                    System.out.println("======================");
+                    break;
+
+                case "P":
+                    displayNegativeEntries();
+                    System.out.println("======================");
+                    break;
+
+                case "p":
+                    displayNegativeEntries();
+                    System.out.println("======================");
+                    break;
+
+                case "R":
+                    reportsMenu();
+                    System.out.println("======================");
+                    break;
+
+                case "r":
+                    reportsMenu();
+                    System.out.println("======================");
+                    break;
+
+                case "H":
+                    return;
+
+                case "h":
+                    return;
+            }
+
+
+        } while (ledgerCommand != "H" || ledgerCommand != "h");
     }
 
+    private static void displayEntries() {
+        System.out.println("displayEntries works.");
+    }
 
+    private static void displayDEntries() {
+        System.out.println("displayDEntries works.");
+    }
+
+    private static void displayNegativeEntries() {
+        System.out.println("displayNegativeEntries works.");
+    }
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+    // Reports Menu:
+    private static void reportsMenu() {
+        System.out.println("Loading ledger: ");
+        String reportMenu = "Choose an option: \n 1) Month to Date\n 2) Previous Month\n 3) Year to Date\n 4) Previous Year\n 5) Search by Vendor\n 0) Back";
+        int reportCommand;
+        do{
+            System.out.println(reportMenu);
+            reportCommand = ConsoleHelper.promptForInt("Enter your command");
+            switch (reportCommand){
+                case 1:
+                    monthToDate();
+                    System.out.println("======================");
+                    break;
+
+                case 2:
+                    previousMonth();
+                    System.out.println("======================");
+                    break;
+
+                case 3:
+                    yearToDate();
+                    System.out.println("======================");
+                    break;
+
+                case 4:
+                    previousYear();
+                    System.out.println("======================");
+                    break;
+
+                case 5:
+                    searchByVendor();
+                    System.out.println("======================");
+                    break;
+
+                case 0:
+                    return;
+            }
+
+
+        } while (reportCommand != 0 || reportCommand != 0);
+    }
+
+    private static void monthToDate() {
+        System.out.println("Month to Date works.");
+    }
+
+    private static void previousMonth() {
+        System.out.println("Previous month works.");
+    }
+
+    private static void yearToDate() {
+        System.out.println("Year to date works.");
+    }
+
+    private static void previousYear() {
+        System.out.println("Previous year works.");
+    }
+
+    private static void searchByVendor() {
+        System.out.println("Search by vendor works.");
+    }
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
     public static ArrayList<Transaction>getTransactionsFromFile(){
         ArrayList<Transaction>transactions = new ArrayList<Transaction>();
+        try{
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String lineFromString;
+
+            while((lineFromString = br.readLine()) != null){
+                String[] parts = lineFromString.split("\\|"); // Splits the total information given at the "|":
+                String desc = parts[0]; // First part that's split.
+                String vendor = parts[1]; // Second part that's split.
+                double amount = Double.parseDouble(parts[2]); // Third part that's split.
+
+                Transaction t = new Transaction(desc, vendor, amount); // Puts the split information in order.
+                transactions.add(t);
+            }
+
+            // Close both the FileReader and BufferedReader.
+            br.close();
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Something is wrong..");
+        }
+
 
 
     return transactions; // Change from null to transactions after changing line 6 from private static to public static.
